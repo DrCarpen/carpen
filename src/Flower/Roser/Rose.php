93a761df47a9046d40fb70d9df71a86233d114ec
nj_liyang <liyang@uniondrug.cn>
2018-11-27 19:57:14 +0800

    init

---
+++ src/Flower/Roser/Rose.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author liyang <liyang@uniondrug.cn>
+ * @date   2018-11-27
+ */
+namespace Flower\Rose;
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
